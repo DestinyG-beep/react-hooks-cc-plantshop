aa5bd9445d4b27b0186c569c7324c65b575d2429
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-11-10 19:14:25 +0300

    fetch

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useEffect} from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, setPlants, result }) {
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(data => setPlants(data))
+  }, [])
+
+ 
+
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
